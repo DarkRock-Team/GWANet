d1e95bbc08985b13893ee0d3de471fbaf5c36653
Madpixel6 <piotrrbulka@gmail.com>
2021-04-18 17:36:27 +0200

    Fixed namespaces

---
+++ Source/HookManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GWANet
+{
+    /// <summary>
+    /// Provides abstraction layer between GWANet logic and hooking library used (MinHook.NET)
+    /// </summary>
+    internal class HookManager
+    {
+
+    }
+}
--- Source/MemScanner.cs
+++ Source/MemScanner.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-namespace GWANet.Source
+namespace GWANet
 {
     internal class MemScanner
     {
