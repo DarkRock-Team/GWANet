adbba8e0bc54707577e4be66e45e9fddfe315624
Piotr <piotrrbulka@gmail.com>
2023-02-16 20:52:23 +0100

    SpanSlitEnumerator unit tests

--- Tests/GWANet.Scanner.UnitTests/Extensions/SpanSplitEnumeratorTests.cs
+++ Tests/GWANet.Scanner.UnitTests/Extensions/SpanSplitEnumeratorTests.cs
@@ -8,13 +8,25 @@ public sealed class SpanSplitEnumeratorTests
     [Fact]
     public void MoveNext_Should_SetCurrent()
     {
+        var enumerator = new SpanSplitEnumerator<char>("F0 AB B2 ?? 71", ' ');
+        var currentItem = enumerator.Current;
         
+        enumerator.MoveNext();
+
+        Assert.True(currentItem.IsEmpty is false);
+        Assert.True(currentItem.Length == 2);
+        Assert.True(currentItem.ToString().Equals("F0"));
     }
 
     [Theory]
     [InlineData("F0 AB B2 ?? 71", ' ')]
-    public void SpanSplitEnumerator(string stringPattern, char splitItem)
+    public void SpanSplitEnumeratorCurrent_Should_BeValid(string stringPattern, char splitItem)
     {
         var enumerator = new SpanSplitEnumerator<char>(stringPattern, splitItem);
+
+        var currentItem = enumerator.Current;
+
+        Assert.True(currentItem.IsEmpty is false);
+        Assert.True(currentItem.Length == 14);
     }
 }
